05f9a5785af50acd665fb134d24de5781aed3b21
valeriacardenasf <v.cardenasf@uniandes.edu.co>
2025-05-24 20:33:07 -0500

    Importar librerias

--- Tablero/Dash.py
+++ Tablero/Dash.py
@@ -1,1 +1,9 @@
 #Dash
+
+#Importar librerias
+import dash
+from dash import html, dcc, Input, Output, State
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+import tensorflow as tf
