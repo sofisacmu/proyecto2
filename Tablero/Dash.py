6ec08c7cf371a6fa88309b23df424460d32965aa
valeriacardenasf <v.cardenasf@uniandes.edu.co>
2025-05-24 20:34:59 -0500

    Cargar el csv de los datos y el modelo

--- Tablero/Dash.py
+++ Tablero/Dash.py
@@ -7,3 +7,8 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
 import tensorflow as tf
+
+
+# Cargar los datos y el modelo
+df = pd.read_csv("/Users/valeriacardenas/Desktop/proyecto2/Tablero/Datos2011Limipios.csv")
+modelo = tf.keras.models.load_model("/Users/valeriacardenas/Desktop/proyecto2/Modelamiento/modelo_ej.keras")
